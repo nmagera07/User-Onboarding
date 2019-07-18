532c4913258e06a4ceed6678d6726eb3a472d48b
Nate Magera <nmagera07@gmail.com>
2019-07-18 10:00:32 -0400

    styling changes to the app

--- user/src/App.js
+++ user/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Form from './components/Form'
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
     <div className="App">
+      <h2>Welcome to Formik Forms!</h2>
+      <img src="https://cdn.mos.cms.futurecdn.net/EzgdmaCQuT84bgDL4fhXZS.jpg" alt="javascript fun"/>
       <Form />
     </div>
   );
--- user/src/components/Form.jsx
+++ user/src/components/Form.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { withFormik, Form, Field } from 'formik'
 import * as Yup from "yup"
 import axios from 'axios'
+import { Modal, Button, Image } from 'semantic-ui-react'
+
 
 const AppForm = ({errors, props, values, touched, isSubmitting}) => {
+
+
     return ( 
+        <div>
+            
+            <Modal size="large" centered trigger={<button>Click to Sign Up</button>} >
+                {/* <Image wrapped size="big" src="https://cdn.mos.cms.futurecdn.net/EzgdmaCQuT84bgDL4fhXZS.jpg" /> */}
+                <Modal.Header>Sign Up Below!</Modal.Header>
             <Form>
                 <div>
                     {touched.name && errors.name && <p>{errors.name}</p>}
@@ -15,15 +24,25 @@ const AppForm = ({errors, props, values, touched, isSubmitting}) => {
                     {touched.password && errors.password && <p>{errors.password}</p>}
                 <Field type="password" name="password" placeholder="password" />
                 </div>
+                <label>
+                    Gender: 
+                <Field component="select" name="gender">
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="not-defined">Prefer not to answer</option>
+                </Field>
+                </label>
                 <div>
                     {touched.terms && errors.terms && <p>{errors.terms}</p>}
-                <label>
+                <label className="checkbox">
                 <Field type="checkbox" name="terms" checked={values.terms}/> 
                 Terms of Service
                 </label>
                 </div>
                 <button disabled={isSubmitting}>Submit</button>
             </Form>
+            </Modal>
+            </div>
      );
 }
 
